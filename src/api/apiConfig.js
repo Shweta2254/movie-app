bb587116c7d156d2c242ae75db8714a16d6b462a
Shweta Srivastava <81436968+Shweta2254@users.noreply.github.com>
2022-06-08 18:37:43 +0530

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '9025041f851d172cd3d42ea8491c076b',
+    apiKey: '',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
